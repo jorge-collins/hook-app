0875a469c0ecc939c1833d62fcb203ad5893b4c4
jorge-collins <jorge.collins@gmail.com>
2021-07-30 20:23:17 -0600

    Clase 110 FormWithCustomHook

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
 
-    <SimpleForm />,
+    <FormWithCustomHook />,
   document.getElementById('root')
 );
  
